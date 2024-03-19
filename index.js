55e337c42441da259f4290e390052fb4830c352e
baldx <mr.ahmad.m2006@gmail.com>
2024-03-19 12:48:27 +0100

    add event to scroll & labels

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const scrollAboutUs = document.querySelector('.about-link');
+    const scrollAboutUs = document.querySelector('.about-link'); //get .about-links
     
-    scrollAboutUs.addEventListener("click", e => {
+    scrollAboutUs.addEventListener("click", e => { //click event to all links
         e.preventDefault();
-        const target = scrollAboutUs.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(target);
+        const target = scrollAboutUs.getAttribute('href').substring(1); //targets the href attribute but removes the string before
+        const targetElement = document.getElementById(target); //targets the element
 
-        if (targetElement) {
-            targetElement.scrollIntoView({ behavior: "smooth" });
+        if (targetElement) { //if element exist
+            targetElement.scrollIntoView({ behavior: "smooth" }); //scroll to element
         }
     })
 })
+
+// const formOutline = document.querySelectorAll('.form-outline');
+// const labels = document.querySelectorAll('label');
+
+// const dataInput = document.querySelectorAll('data-input');
+
+
+// formOutline.forEach(e => e.addEventListener('click', () => {
+//     labels.forEach(label => label.remove())
+// }));
